6678de9d5c82e50046b7aff280502ee31f7d3033
DuckMankie <dhdmanh23@clc.fitus.edu.vn>
2025-05-27 13:14:06 +0700

    update assets

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((400, 400))
+pygame.display.set_caption("8-Puzzle")
+
+icon = pygame.image.load("icon_32.png")
+pygame.display.set_icon(icon)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+
+pygame.quit()
